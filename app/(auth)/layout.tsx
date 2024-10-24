396a6c71b2eb825e30adc73aa47298e526fa1bde
Jerome Doran <disneyfan9184@gmail.com>
2024-10-24 16:58:41 -0400

    finished sign in and sign up form layouts

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
 import { ModeToggle } from '@/components/mode-toggle';
 import { Button } from '@/components/ui/button';
 
@@ -7,13 +11,19 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isSignIn = pathname === '/sign-in';
   return (
     <main className='dark:bg-neutral-900 bg-neutral-100 h-screen'>
       <div className='mx-auto max-w-screen-2xl p-4'>
         <nav className='flex justify-between items-center'>
           <Image src='/images/icon.png' alt='Logo' width={50} height={50} />
           <div className='flex gap-4 items-center'>
-            <Button variant='secondary'>Sign Up</Button>
+            <Button asChild variant='secondary'>
+              <Link href={isSignIn ? '/sign-up' : '/sign-in'}>
+                {isSignIn ? 'Sign up' : 'Login'}
+              </Link>
+            </Button>
             <ModeToggle />
           </div>
         </nav>
--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -1,3 +1,6 @@
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import DottedSeparator from '@/components/dotted-separator';
 import { Button } from '@/components/ui/button';
 import {
@@ -11,8 +14,33 @@ import { Input } from '@/components/ui/input';
 import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, 'Name is required'),
+  email: z.string().email(),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+});
 
 export default function SignUpCard() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log({ values });
+  }
   return (
     <Card className='w-full h-full md:w-[487px] border-none shadow-none'>
       <CardHeader className='flex items-center justify-center text-center p-7'>
@@ -32,37 +60,61 @@ export default function SignUpCard() {
         <DottedSeparator color='#4d4d4e' />
       </div>
       <CardContent className='p-7'>
-        <form className='space-y-4'>
-          <Input
-            required
-            type='text'
-            value={''}
-            onChange={() => {}}
-            placeholder='Enter your name'
-            disabled={false}
-          />
-          <Input
-            required
-            type='email'
-            value={''}
-            onChange={() => {}}
-            placeholder='Enter email address'
-            disabled={false}
-          />
-          <Input
-            required
-            type='password'
-            value={''}
-            onChange={() => {}}
-            placeholder='Enter password'
-            disabled={false}
-            min={8}
-            max={256}
-          />
-          <Button disabled={false} size='lg' className='w-full'>
-            Sign up
-          </Button>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+            <FormField
+              name='name'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type='text'
+                      placeholder='Enter your name'
+                      {...field} // prevents having to pass form properties manually when spreading the field prop
+                    />
+                  </FormControl>
+                  <FormMessage className='text-orange-500' />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='email'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type='email'
+                      placeholder='Enter email address'
+                      {...field} // prevents having to pass form properties manually when spreading the field prop
+                    />
+                  </FormControl>
+                  <FormMessage className='text-orange-500' />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='password'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type='password'
+                      placeholder='Enter password'
+                      {...field} // prevents having to pass form properties manually when spreading the field prop
+                    />
+                  </FormControl>
+                  <FormMessage className='text-orange-500' />
+                </FormItem>
+              )}
+            />
+            <Button disabled={false} size='lg' className='w-full'>
+              Sign up
+            </Button>
+          </form>
+        </Form>
       </CardContent>
       <div className='px-7'>
         <DottedSeparator color='#4d4d4e' />
@@ -86,6 +138,17 @@ export default function SignUpCard() {
           <FaGithub className='size-6' />
           Sign up with Github
         </Button>
+      </CardContent>
+      <div className='px-7'>
+        <DottedSeparator color='#4d4d4e' />
+      </div>
+      <CardContent className='p-7 flex items-center justify-center'>
+        <p className='text-sm'>
+          Already have an account?
+          <Link href='/sign-in'>
+            <span className='text-blue-600'> Sign in</span>
+          </Link>
+        </p>
       </CardContent>
     </Card>
   );
